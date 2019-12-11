5d617b9301b5e71751fdfc4741f4b476ddbf0ea6
Aditya <adityakadam@brandzgarage.com>
2019-12-11 23:01:44 +0530

    fixed db connections integrated list api

--- Backend/config.js
+++ Backend/config.js
@@ -1,12 +1,17 @@
 const mongoose = require("mongoose");
 
-const db = "mongodb+srv://admin:5ot2D05YCg1cJKby@cluster0-kax9l.mongodb.net/test?retryWrites=true&w=majority";
-mongoose.connect(db,{ useNewUrlParser: true, useUnifiedTopology: true }, err => {
-  if (err) {
-    console.log("MongoDB connection failed", err);
-  } else {
-    console.log("Suucessfully connected with mongo db..!");
+const db =
+  "mongodb+srv://admin:D0GMouXjd4CkrrjX@cluster0-kax9l.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(
+  db,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  err => {
+    if (err) {
+      console.log("MongoDB connection failed", err);
+    } else {
+      console.log("Suucessfully connected with mongo db..!");
+    }
   }
-});
+);
 
 module.exports = mongoose;
